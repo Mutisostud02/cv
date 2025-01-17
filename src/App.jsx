862895a06deb7221f5a3ede79e0bb1e450c75ef4
MutisoStud02 <mutiso8395@gmail.com>
2025-01-17 10:56:05 +0300

    Removed subform components and replaced with their parent component MainForm in a display div tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import Header from "./components/header"
-import MainTags from "./components/mainTag"
-import Aside from "./components/aside"
 import PersonalDetailsForm from "./components/personalDetailsForm"
 import EducationForm from "./components/educationForm"
 import WorkExpForm from "./components/workExperienceForm"
+import MainForm from "./components/main.Form"
+
+
 function App() {
 
   return (
     <>
      <Header/>
-     <div className="content-box">
-      <Aside>
-        <PersonalDetailsForm/>
-        <EducationForm/>
-        <WorkExpForm/>
-      </Aside>
-      <MainTags/>
-    </div>
-       
+     <div className="display">
+     <MainForm/>   
+     </div>    
     </>
   )
 }
